e33fed49ccade3c50afd1996ed39d732e398690d
MiqueasGC <miqueasc.garcia@gmail.com>
2026-09-20 18:09:03 -0300

    proyecto 2

---
+++ proyecto-2/src/index.js
@@ -0,0 +1,9 @@
+const _ = require("lodash");
+
+const numeros = [10, 20, 30];
+
+console.log("Segundo proyecto de la PoC");
+console.log("Números:", numeros);
+console.log("Mínimo:", _.min(numeros));
+console.log("Máximo:", _.max(numeros));
+console.log("Promedio:", _.mean(numeros));
